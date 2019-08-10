4aac51108e6617f0f64766879862db9604dfefa4
Alexander <dev@salex.pro>
2019-08-10 06:24:51 +0300

    subscribe form & content fixes

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,19 @@
 // eslint-disable-next-line no-unused-vars
 // import polyfill from '@babel/polyfill';
+import $ from 'jquery';
+
 
 import svg4everybody from 'svg4everybody';
 import './lib/foundation-explicit-pieces';
 
 svg4everybody();
+
+$(document).scroll(() => {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop >= 600){
+        $('.up').addClass('is_visible');
+    } else {
+        $('.up').removeClass('is_visible');
+    }
+
+})
